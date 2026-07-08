6785f3cc64f33232d4d4050fd81d50c6d2c5a2bd
Adeshina <toyyibadeshina4@gmail.com>
2026-07-08 17:05:45 +0100

    Add grading logic with if/elif/else to grade.py

---
+++ grade.py
@@ -0,0 +1,12 @@
+score = int(input("Score: "))
+if score >= 90  <= 100:
+    print("Grade: A")  
+elif score >= 80  < 90:
+    print("Grade: B")
+elif score >= 70  < 80:
+    print("Grade: C")
+elif score >= 60  < 70:
+    print("Grade: D")
+          
+else:
+    print("Grade: F")   
